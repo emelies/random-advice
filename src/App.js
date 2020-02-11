02e57e2c90416998205f4917ff5aff7f3cf6ef0e
emelies <emelie.sabel@hyperisland.se>
2020-02-11 11:23:13 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AdviceComponent from "./advice-component"
+
+function App() {
+  return (
+    <div className="App">
+      <AdviceComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/advice-component.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function AdviceComponent() { 
+    
+  const [advice, onChangeAdvice] = useState();
+  React.useEffect(() => {
+    if (advice) {
+      return;
+    }
+
+    fetch("https://api.adviceslip.com/advice") // fetching the data
+      .then(res => res.json()) // turn response into JSON
+      .then(data => {
+        const randomAdvice = data.slip.advice; // declaring var to store the specific data we want. in this case - advice from API
+
+        onChangeAdvice(randomAdvice); // onChange tells the computer to update advice from the const outside the useEffect. and inside onChange tells what to update advice with.
+      }); // what data and what within the data to update. in this ex its ADVICE from the const, outside the useEffect
+  });
+
+  return (
+    <div>
+      <div>{advice}</div>
+      {/* <button onClick={}>refresh advice</button> */}
+    </div>
+  );
+}
+
+export default AdviceComponent;
